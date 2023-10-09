79576dee98a3121168e057e9f0eeab9eb7693918
minazuki <aratanotennis@gmail.com>
2023-10-10 05:08:52 +0900

    数字を追加

---
+++ hello5/src/main/java/com/minazuki/hello/HelloController.java
@@ -0,0 +1,20 @@
+package com.minazuki.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Map<String, Object> hello() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Hello world");
+        response.put("number", 42); // 適当なint型の数字を追加
+        return response;
+
+    }
+
+}
